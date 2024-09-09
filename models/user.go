bd2da40fec4e4bd60302a03a61e75eff325bb857
fiorotticaio <caiofiorotti@gmail.com>
2024-09-09 09:54:05 -0300

    :lock: creating set password function

--- models/user.go
+++ models/user.go
@@ -1,10 +1,20 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id        uint   `json:"id"`
-	FisrtName string `json:"first_name"`
+	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Password  []byte `json:"-"` // Hide the password from JSON
 	Phone     string `json:"phone"`
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14) // 14 is the cost of the hashing
+	if err != nil {
+		panic("Could not hash the password")
+	}
+	user.Password = hashedPassword
 }
