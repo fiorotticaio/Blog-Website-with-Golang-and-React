598e7732342ad60fb5d6e13bb37af91125fb1892
fiorotticaio <caiofiorotti@gmail.com>
2024-09-09 09:16:19 -0300

    :mag: migrating user model to databse

--- database/connect.go
+++ database/connect.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"os"
 
+	"github.com/fiorotticaio/Blog-Website-with-Golang-and-React/models"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
-	"gorm.io/gorm"	
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
@@ -17,7 +18,7 @@ func Connect() {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := os.Getenv("DSN") // Get DSN from .env file
+	dsn := os.Getenv("DSN")                                     // Get DSN from .env file
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) // Connect to MySQL
 	if err != nil {
 		panic("Could not connect to the database")
@@ -26,4 +27,9 @@ func Connect() {
 	}
 
 	DB = database // Set the global DB variable to the database connection
+
+	// Migrate the models (tables) to the database
+	database.AutoMigrate(
+		&models.User{},
+	)
 }
