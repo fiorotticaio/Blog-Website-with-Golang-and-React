7060b38e77f3baaf62a2fbbe871216164ba4adf4
Matheus <mt.schreiber01@gmail.com>
2024-09-10 12:18:07 -0300

    :fire: add frontend module

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Register from "./components/register";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import CreateBlog from "./components/CreateBlog";
+import Navbar from "./components/Navbar";
+import BlogDetail from "./components/BlogDetails";
+import PersonalBlog from "./components/PersonalBlog";
+import EditPost from "./components/EditPost";
+function App() {
+  return (
+    <div className="">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/create" element={<CreateBlog />} />
+          <Route exact path="/detail/:id" element={<BlogDetail />} />
+          <Route exact path="/personal" element={<PersonalBlog />} />
+          <Route exact path="/edit/:id" element={<EditPost />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
